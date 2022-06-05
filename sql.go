6adb671b8681113830bc961efe62d3551c4aed72
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-05 08:24:09 -0600

    Reconnect to DB on error

--- sql.go
+++ sql.go
@@ -11,9 +11,17 @@ import (
 
 func db() *sql.DB {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4&collation=utf8mb4_0900_ai_ci", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASS"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT"), os.Getenv("MYSQL_DATABASE"))
-	db, err := sql.Open("mysql", dsn)
 
-	maybePanic(err)
+	var err error
+	for ok := true ; ok ; err = nil {
+		db, err := sql.Open("mysql", dsn)
+
+		if err != nil {
+			fmt.Println("Error connecting to database:", err)
+			time.Sleep(time.Second)
+			continue
+		}
+	}
 
 	db.SetConnMaxLifetime(time.Second * 5)
 	db.SetMaxOpenConns(10)
