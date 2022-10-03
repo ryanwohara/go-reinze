4f37e1bf828a159ddb3a27d9c92765b63cd583cb
Ryan William O'Hara <ryan@ryanwohara.com>
2022-10-02 22:39:28 -0600

    chore: "to_post" table in sql schema

--- rsnews.sql
+++ rsnews.sql
@@ -14,3 +14,12 @@ CREATE TABLE `reinze`.`news` (
   `hash_id` VARCHAR(5) NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT now(),
   PRIMARY KEY (`id`));
+
+CREATE TABLE `reinze`.`to_post` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `path_on_disk` varchar(125) NOT NULL,
+  `scheduled_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `posted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
