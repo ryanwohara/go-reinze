f824d6473b5a664723a4ab3862693e93f3f4c093
Ryan William O'Hara <ryan@ryanwohara.com>
2022-10-03 16:12:24 -0600

    feat: Actions as greets

--- greet/greet.go
+++ greet/greet.go
@@ -1,1 +1,17 @@
-//
+package greet
+
+import (
+	"math/rand"
+	"os"
+	"strings"
+
+	irc "github.com/thoj/go-ircevent"
+)
+
+func Greet(irccon *irc.Connection, channel string, nick string) {
+	greets := strings.Split(os.Getenv("GREET_MESSAGES"), "\n")
+
+	greet := strings.Replace(greets[rand.Intn(len(greets)-1)], "!nick!", nick, -1)
+
+	irccon.Action(channel, greet)
+}
--- join.go
+++ join.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
+	"github.com/ryanwohara/reinze/greet"
 	irc "github.com/thoj/go-ircevent"
 )
 
 func addJoin(irccon *irc.Connection) {
 	irccon.AddCallback("JOIN", func(event *irc.Event) {
-		fmt.Println("~~>", event.Arguments[0])
-		if event.Arguments[0] == "#asdfghj" {
-			irccon.Privmsg(event.Arguments[0], "foo")
+		if event.Arguments[0] == os.Getenv("GREET_CHANNEL") {
+			greet.Greet(irccon, event.Arguments[0], event.Nick)
+			greet.Greet(irccon, event.Arguments[0], event.Nick)
 		}
 	})
 }
