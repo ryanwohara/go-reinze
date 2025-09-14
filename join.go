ff37d1365744ff12eccb53ba4bd0ad4b5d049eac
Ryan William O'Hara <ryan@ryanwohara.com>
2025-09-14 11:45:43 -0600

    fix: Do not greet itself

--- join.go
+++ join.go
@@ -9,7 +9,7 @@ import (
 
 func addJoin(irccon *irc.Connection) {
 	irccon.AddCallback("JOIN", func(event *irc.Event) {
-		if event.Arguments[0] == os.Getenv("GREET_CHANNEL") {
+		if event.Arguments[0] == os.Getenv("GREET_CHANNEL") && os.Getenv("IRC_NICK") != event.Nick {
 			greet.Greet(irccon, event.Arguments[0], event.Nick)
 			greet.Greet(irccon, event.Arguments[0], event.Nick)
 		}
