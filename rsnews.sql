26e818fbf62a95fd9dd9fecc3856646f1df2e6b7
Ryan William O'Hara <ryan@ryanwohara.com>
2025-09-24 21:07:08 -0600

    fix: Missing index

--- rsnews.sql
+++ rsnews.sql
@@ -15,17 +15,4 @@ CREATE TABLE `reinze`.`news` (
   `created_at` TIMESTAMP NOT NULL DEFAULT now(),
   PRIMARY KEY (`id`));
 
-CREATE TABLE `reinze`.`to_post` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `path_on_disk` varchar(125) NOT NULL,
-  `scheduled_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `posted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `reinze`.`twitter` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `text` VARCHAR(255) NOT NULL,
-  `tweet_id` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`id`));
+ALTER TABLE news ADD INDEX `url_index` (`url`);
